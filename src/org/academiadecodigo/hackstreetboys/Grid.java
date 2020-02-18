4ada71b3e1394415e804e0033a2a3b60000944b0
Lucas Centeno <centenocode@gmail.com>
2020-02-18 09:12:04 +0000

    we have a white grid

--- src/org/academiadecodigo/hackstreetboys/Grid.java
+++ src/org/academiadecodigo/hackstreetboys/Grid.java
@@ -1,4 +1,42 @@
 package org.academiadecodigo.hackstreetboys;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Grid {
+
+    public static final int PADDING = 10;
+    private int cols;
+    private int rows;
+    private int cellSize = 10;
+
+    private Rectangle rectangle;
+
+    Grid (int cols, int rows){
+        this.cols = cols;
+        this.rows = rows;
+    }
+
+    public void init() {
+        rectangle = new Rectangle(PADDING,PADDING,cols*cellSize,rows*cellSize);
+        rectangle.draw();
+    }
+
+    public int getCols() { return cols; }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getWidth() { return this.rectangle.getWidth(); }
+
+    public int getHeight() {return this.rectangle.getHeight(); }
+
+    public int getX() {return rectangle.getX();}
+
+    public int getY() { return rectangle.getY();}
+
+    public int getCellSize() { return cellSize; }
+
+    public int rowToY(int row) { return PADDING + cellSize * row; }
+
+    public int columnToX(int column) { return PADDING + cellSize * column; }
 }
--- src/org/academiadecodigo/hackstreetboys/Main.java
+++ src/org/academiadecodigo/hackstreetboys/Main.java
@@ -3,5 +3,7 @@ package org.academiadecodigo.hackstreetboys;
 public class Main {
     public static void main(String[] args) {
 
+        Grid newGrid = new Grid(100,100) ;
+        newGrid.init();
     }
 }
