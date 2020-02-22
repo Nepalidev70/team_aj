a7dc3a68a4420f81847dec939064d6363e0c8738
Ricardo Flores <ricardo.flores@netcabo.pt>
2020-02-22 18:06:08 +0000

    Removed Obstacle class and converted to single method || created road piece container for the track and created isOnRoad method

---
+++ src/org/academiadecodigo/hackstreetboys/grid/Grid.java
@@ -0,0 +1,131 @@
+package org.academiadecodigo.hackstreetboys.grid;
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+import java.util.List;
+
+public class Grid {
+
+    public static final int PADDING = 10;
+    private int cols;
+    private int rows;
+    private int cellSize = 10;
+
+    private Rectangle rectangle;
+
+    private List<Rectangle> roadRectangleContainer;
+
+    public Grid(int cols, int rows) {
+        this.cols = cols;
+        this.rows = rows;
+    }
+
+    public void init(Color roadColor) {
+        Picture background = new Picture(0,0,"resources/backgroundLisbon.png");
+        background.grow(00,00);
+        background.draw();
+        buildRoad(roadColor);
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getWidth() {
+        return this.rectangle.getWidth();
+    }
+
+    public int getHeight() {
+        return this.rectangle.getHeight();
+    }
+
+    public int getX() {
+        return rectangle.getX();
+    }
+
+    public int getY() {
+        return rectangle.getY();
+    }
+
+    public int getCellSize() {
+        return cellSize;
+    }
+
+    public int rowToY(int row) {
+        return PADDING + cellSize * row;
+    }
+
+    public int columnToX(int column) {
+        return PADDING + cellSize * column;
+    }
+
+
+    private void buildRoad(Color color) {
+
+        Rectangle rectangle1 = new Rectangle(00, 305, 332, 40);
+        rectangle1.setColor(color);
+        rectangle1.fill();
+        roadRectangleContainer.add(rectangle1);
+
+        Rectangle rectangle2 = new Rectangle(330, 44, 24, 300);
+        rectangle2.setColor(color);
+        rectangle2.fill();
+        roadRectangleContainer.add(rectangle2);
+
+        Rectangle rectangle3 = new Rectangle(329, 05, 329, 40);
+        rectangle3.setColor(color);
+        rectangle3.fill();
+        roadRectangleContainer.add(rectangle3);
+
+        Rectangle rectangle4 = new Rectangle(658, 05, 24, 600);
+        rectangle4.setColor(color);
+        rectangle4.fill();
+        roadRectangleContainer.add(rectangle4);
+
+        Rectangle rectangle5 = new Rectangle(658, 600, 178, 40);
+        rectangle5.setColor(color);
+        rectangle5.fill();
+        roadRectangleContainer.add(rectangle5);
+
+        Rectangle rectangle6 = new Rectangle(832, 600, 24, 300);
+        rectangle6.setColor(color);
+        rectangle6.fill();
+        roadRectangleContainer.add(rectangle6);
+
+        Rectangle rectangle7 = new Rectangle(832, 890, 178, 40);
+        rectangle7.setColor(color);
+        rectangle7.fill();
+        roadRectangleContainer.add(rectangle7);
+
+        Rectangle rectangle8 = new Rectangle(1010, 590, 35, 340);
+        rectangle8.setColor(color);
+        rectangle8.fill();
+        roadRectangleContainer.add(rectangle8);
+
+        Rectangle rectangle9 = new Rectangle(1010, 590, 155, 50);
+        rectangle9.setColor(color);
+        rectangle9.fill();
+        roadRectangleContainer.add(rectangle9);
+
+    }
+
+    public boolean isOnRoad(int nextX, int nextY){
+        for(Rectangle road : roadRectangleContainer){
+            if(nextX < road.getX() || nextX > road.getX() + road.getWidth() || nextY < road.getY() || nextY > road.getY() + road.getHeight() ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
+
+
+
