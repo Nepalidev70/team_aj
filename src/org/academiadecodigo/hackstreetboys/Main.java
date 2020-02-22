54ff8c406a2a4e2641f8891fafa61d1609af3079
Ricardo Flores <ricardo.flores@netcabo.pt>
2020-02-22 18:23:14 +0000

    Merged feature and changed main

--- src/org/academiadecodigo/hackstreetboys/Main.java
+++ src/org/academiadecodigo/hackstreetboys/Main.java
@@ -1,14 +1,13 @@
 package org.academiadecodigo.hackstreetboys;
 
+import org.academiadecodigo.hackstreetboys.grid.Grid;
 import org.academiadecodigo.simplegraphics.graphics.Color;
 
 public class Main {
     public static void main(String[] args) {
 
         Grid newGrid = new Grid(100, 100);
-        newGrid.init();
-        Obstacles obstacle = new Obstacles();
-        obstacle.buildObstacles(Color.PINK);
+        newGrid.init(Color.BLACK);
         Car car = new Car();
         car.buildCar(Color.BLUE);
     }
