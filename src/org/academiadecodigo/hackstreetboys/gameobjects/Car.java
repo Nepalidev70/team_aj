c5c932f093b9a7583670985d26f851acbb637abc
Ricardo Flores <ricardo.flores@netcabo.pt>
2020-02-23 00:13:09 +0000

    ealy version

--- src/org/academiadecodigo/hackstreetboys/gameobjects/Car.java
+++ src/org/academiadecodigo/hackstreetboys/gameobjects/Car.java
@@ -10,13 +10,52 @@ public class Car {
 
     private Picture raceCarPicture;
     private Direction currentDirection;
+    private Rectangle collisionDetection;
 
+    public int getX(){return collisionDetection.getX();}
 
+    public int getY(){return collisionDetection.getY();}
 
     public void buildCar(Direction direction, int xPosition, int yPosition) {
         this.currentDirection = direction;
-        this.raceCarPicture = new Picture(xPosition, yPosition, "resources/smallRacecar.jpg");
+        setCurrentDirection(direction,xPosition,yPosition);
         this.raceCarPicture.draw();
+        makeFrontBumper(xPosition,yPosition);
+    }
+
+    private void makeFrontBumper(int xPosition, int yPosition){
+        this.collisionDetection = new Rectangle((double)xPosition + 10,(double)yPosition,10,10);
+        this.collisionDetection.setColor(Color.BLACK);
+        this.collisionDetection.draw();
+    }
+
+    public void setCurrentDirection(Direction newDirection,int xPosition, int yPosition){
+        switch (newDirection){
+            case E:
+                this.raceCarPicture = new Picture(xPosition,yPosition,"resources/smallCarE.png");
+                break;
+            case SE:
+                this.raceCarPicture = new Picture(xPosition,yPosition,"resources/smallCarSE.png");
+                break;
+            case S:
+                this.raceCarPicture = new Picture(xPosition,yPosition,"resources/smallCarS.png");
+                break;
+            case SW:
+                this.raceCarPicture = new Picture(xPosition,yPosition,"resources/smallCarSW.png");
+                break;
+            case W:
+                this.raceCarPicture = new Picture(xPosition,yPosition,"resources/smallCarW.png");
+                break;
+            case NW:
+                this.raceCarPicture = new Picture(xPosition,yPosition,"resources/smallCarNO.png");
+                break;
+            case N:
+                this.raceCarPicture = new Picture(xPosition,yPosition,"resources/smallCarN.png");
+                break;
+            case NE:
+                this.raceCarPicture = new Picture(xPosition,yPosition,"resources/smallCarNE.png");
+                break;
+        }
     }
 
     //testing purposes methods
--- src/org/academiadecodigo/hackstreetboys/moveLogic/MovementLogic.java
+++ src/org/academiadecodigo/hackstreetboys/moveLogic/MovementLogic.java
@@ -2,12 +2,56 @@ package org.academiadecodigo.hackstreetboys.moveLogic;
 
 import org.academiadecodigo.hackstreetboys.gameobjects.Car;
 import org.academiadecodigo.hackstreetboys.grid.Track;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+import java.awt.*;
 
 public final class MovementLogic {
 
     public static boolean move(Car car, Track currentTrack){
-
+        //int
+        //if(currentTrack.isOnRoad())
         return false;
+    }
+
+    public static int xDirectionalMove(Direction direction, Rectangle rectangle){
+        switch(direction){
+            case E:
+                return rectangle.getX() + 10;
+            case SE:
+                return rectangle.getX() + 10;
+            case S:
+                return rectangle.getX();
+            case SW:
+                return rectangle.getX() - 10;
+            case W:
+                return rectangle.getX() - 10;
+            case NW:
+                return rectangle.getX() - 10;
+            case N:
+                return rectangle.getX();
+        }
+        return rectangle.getX()+10;
+    }
+
+    public static int yDirectionalMove(Direction direction, Rectangle rectangle){
+        switch(direction){
+            case E:
+                return rectangle.getY();
+            case SE:
+                return rectangle.getY() + 10;
+            case S:
+                return rectangle.getY() + 10;
+            case SW:
+                return rectangle.getY() + 10;
+            case W:
+                return rectangle.getY();
+            case NW:
+                return rectangle.getY() - 10;
+            case N:
+                return rectangle.getY() - 10;
+        }
+        return rectangle.getX()+10;
     }
 
 }
