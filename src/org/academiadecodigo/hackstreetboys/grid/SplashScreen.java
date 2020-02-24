0f09caceb4e12d03d82138e7ffac21e60153bb13
Lucas Centeno <centenocode@gmail.com>
2020-02-24 04:27:02 +0000

    SplashScreen and spacebar init

--- src/org/academiadecodigo/hackstreetboys/grid/SplashScreen.java
+++ src/org/academiadecodigo/hackstreetboys/grid/SplashScreen.java
@@ -1,28 +1,37 @@
 package org.academiadecodigo.hackstreetboys.grid;
 
 
-import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.hackstreetboys.GameLogic;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
-public class SplashScreen {
+public class SplashScreen implements KeyboardHandler {
 
-    public static final int PADDING = 10;
-    private int cols;
-    private int rows;
-    private int cellSize = 10;
-    private Rectangle rectangle;
+    private boolean keyPressed;
+    private Picture background = new Picture();
+
+    Keyboard keyboard = new Keyboard(this);
 
     public void init() {
-        while (true) {
+
+        // -- SPACE -- //
+        KeyboardEvent spacePressed = new KeyboardEvent();
+        spacePressed.setKey(KeyboardEvent.KEY_SPACE);
+        spacePressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(spacePressed);
+
+        while (!keyPressed) {
             boolean draw1 = false;
 
             if (!draw1) {
                 try {
                     Thread.sleep(1000);
-                Picture background = new Picture(0, 0, "resources/SplashScreen1.png");
-                background.grow(00, 00);
-                background.draw();
-                draw1 = true;
+                    background.load("resources/SplashScreen1.png");
+                    background.draw();
+                    draw1 = true;
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -31,51 +40,30 @@ public class SplashScreen {
             if (draw1) {
                 try {
                     Thread.sleep(1000);
-                    Picture background2 = new Picture(0, 0, "resources/SplashScreen2.png");
-                    background2.grow(00, 00);
-                    background2.draw();
+                    background.load("resources/SplashScreen2.png");
+                    background.draw();
                     draw1 = false;
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }
+        background.delete();
     }
 
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        switch (keyboardEvent.getKey()) {
 
-    public int getCols() {
-        return cols;
+            case KeyboardEvent.KEY_SPACE:
+                keyPressed = true;
+                GameLogic gameLogic = new GameLogic();
+                break;
+        }
     }
 
-    public int getRows() {
-        return rows;
-    }
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
 
-    public int getWidth() {
-        return this.rectangle.getWidth();
-    }
-
-    public int getHeight() {
-        return this.rectangle.getHeight();
-    }
-
-    public int getX() {
-        return rectangle.getX();
-    }
-
-    public int getY() {
-        return rectangle.getY();
-    }
-
-    public int getCellSize() {
-        return cellSize;
-    }
-
-    public int rowToY(int row) {
-        return PADDING + cellSize * row;
-    }
-
-    public int columnToX(int column) {
-        return PADDING + cellSize * column;
     }
 }
